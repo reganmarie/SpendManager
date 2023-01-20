2dd9690b4b803914595dd2e203ea25aff997ccc4
Regan Tewksbury <regantewksbury@gmail.com>
2023-01-20 10:58:47 -0800

    Feature 15 complete

--- receipts/models.py
+++ receipts/models.py
@@ -13,6 +13,9 @@ class ExpenseCategory(models.Model):
         on_delete=models.CASCADE,
     )
 
+    def __str__(self):
+        return self.name
+
 
 class Account(models.Model):
     name = models.CharField(max_length=100)
@@ -23,6 +26,9 @@ class Account(models.Model):
         related_name="accounts",
         on_delete=models.CASCADE,
     )
+
+    def __str__(self):
+        return self.name
 
 
 class Receipt(models.Model):
@@ -49,3 +55,6 @@ class Receipt(models.Model):
         on_delete=models.CASCADE,
         null=True,
     )
+
+    def __str__(self):
+        return self.vendor
