177c9f665c8a736d7acd33a9f79a6d7264ee67aa
Regan Tewksbury <regantewksbury@gmail.com>
2023-01-19 20:33:44 -0800

    Feature 14 Complete

--- receipts/forms.py
+++ receipts/forms.py
@@ -1,5 +1,5 @@
 from django.forms import ModelForm
-from receipts.models import Receipt
+from receipts.models import Receipt, ExpenseCategory, Account
 
 
 class ReceiptForm(ModelForm):
@@ -12,4 +12,21 @@ class ReceiptForm(ModelForm):
             "category",
             "date",
             "account",
+        ]
+
+
+class CategoryForm(ModelForm):
+    class Meta:
+        model = ExpenseCategory
+        fields = [
+            "name",
+        ]
+
+
+class AccountForm(ModelForm):
+    class Meta:
+        model = Account
+        fields = [
+            "name",
+            "number",
         ]
--- receipts/urls.py
+++ receipts/urls.py
@@ -4,6 +4,8 @@ from receipts.views import (
     create_receipt,
     category_list,
     account_list,
+    create_category,
+    create_account,
 )
 
 urlpatterns = [
@@ -11,4 +13,6 @@ urlpatterns = [
     path("create/", create_receipt, name="create_receipt"),
     path("categories/", category_list, name="category_list"),
     path("accounts/", account_list, name="account_list"),
+    path("categories/create/", create_category, name="create_category"),
+    path("accounts/create/", create_account, name="create_account"),
 ]
