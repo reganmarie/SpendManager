3632ff342a176c6271eba90386f376c5ab5e2eb6
Regan Tewksbury <regantewksbury@gmail.com>
2023-05-11 10:29:46 -0700

    Added Reciept List to Navbar

--- receipts/urls.py
+++ receipts/urls.py
@@ -11,6 +11,7 @@ from receipts.views import (
 urlpatterns = [
     path("", receipt_list, name="home"),
     path("create/", create_receipt, name="create_receipt"),
+    path("receipts/", receipt_list, name="receipt_list"),
     path("categories/", category_list, name="category_list"),
     path("accounts/", account_list, name="account_list"),
     path("categories/create/", create_category, name="create_category"),
