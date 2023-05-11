8640e1092ceb8165bf7b2ada5449a532b3725e04
Regan Tewksbury <regantewksbury@gmail.com>
2023-05-10 17:05:18 -0700

    added colors to the css stylesheet

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    '.receipts/templates/**/*.html'
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
+module.exports = {
+  content: [
+    '.accounts/templates/**/*.html'
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
